3ac6c9e9ab23e13621d9d01cfd4f0772b965ead1
rev16 <wizard6teen@gmail.com>
2022-09-17 22:03:39 +0530

    Sanity.io Backend full setup done.

---
+++ airbnb-clone/schemas/propertyImage.js
@@ -0,0 +1,18 @@
+export default {
+  name: "propertyImage",
+  title: "Property Image",
+  type: "image",
+  fileds: [
+    {
+      name: "caption",
+      title: "Caption",
+      type: "string",
+      options: {
+        isHighLight: true,
+      },
+    },
+  ],
+  options: {
+    hotspot: true,
+  },
+};
